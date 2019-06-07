c0c740787b0305d445e49ca5e69edbea1ea043a9
EC <crensoft@gmail.com>
2019-06-07 17:22:26 -0400

    Use getenv

--- lib/contact.php
+++ lib/contact.php
@@ -11,38 +11,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-function crensoft_contact($data) {
-  if (!validateInputs($data)) {
-    return ['status' => 400];
-  }
-  // Create the Transport
-  $transport = createTransport();
-
-  // Create the Mailer using your created Transport
-  $mailer = new \Swift_Mailer($transport);
-
-  // get values
-  $cleanData = array_map(cleanInput, $data);
-
-  $body = createBody($cleanData);
-
-  // Create a message
-  $message = (new \Swift_Message('Contact Form: ' . $cleanData['name']))
-    ->setFrom([$_ENV['CONTACT_FROM'] => 'Contact Form'])
-    ->setTo([$_ENV['CONTACT_EMAIL'] => $_ENV['CONTACT_NAME']])
-    ->setReplyTo([$cleanData['email'] => $cleanData['name']])
-    ->setBody($body, 'text/html')
-    ->addPart(strip_tags($body), 'text/plain');
-
-  // Send the message
-  $result = $mailer->send($message);
-  $status = 500;
-  if ($result) {
-    $status = 200;
-  }
-  return ['data' => $result, 'status' => $status];
-}
-
 function validateInputs($input) {
   if (!$input['name'] || !$input['email'] || !$input['message']) {
     return false;
@@ -58,13 +26,45 @@ function cleanInput($input) {
 }
 
 function createTransport() {
-  return (new \Swift_SmtpTransport($_ENV['MAIL_HOST'], $_ENV['MAIL_PORT']))
-  ->setUsername($_ENV['MAIL_USER'])
-  ->setPassword($_ENV['MAIL_PASS']);
+  return (new \Swift_SmtpTransport(getenv('MAIL_HOST'), getenv('MAIL_PORT')))
+  ->setUsername(getenv('MAIL_USER'))
+  ->setPassword(getenv('MAIL_PASS'));
 }
 
 function createBody($data) {
   return "<strong>Name:</strong> {$data['name']} ({$data['email']})<br /><br />\n\n"
   . ($data['phone'] ? "<strong>Phone:</strong> {$data['phone']}<br /><br />\n\n" : "")
   . "<strong>Msg:</strong> {$data['message']}";
+}
+
+function crensoft_contact($data) {
+  if (!validateInputs($data)) {
+    return ['status' => 400];
+  }
+  // Create the Transport
+  $transport = createTransport();
+
+  // Create the Mailer using your created Transport
+  $mailer = new \Swift_Mailer($transport);
+
+  // get values
+  $cleanData = array_map(cleanInput, $data);
+
+  $body = createBody($cleanData);
+  
+  // Create a message
+  $message = (new \Swift_Message('Contact Form: ' . $cleanData['name']))
+    ->setFrom([getenv('CONTACT_FROM') => 'Contact Form'])
+    ->setTo([getenv('CONTACT_EMAIL') => getenv('CONTACT_NAME')])
+    ->setReplyTo([$cleanData['email'] => $cleanData['name']])
+    ->setBody($body, 'text/html')
+    ->addPart(strip_tags($body), 'text/plain');
+
+  // Send the message
+  $result = $mailer->send($message);
+  $status = 500;
+  if ($result) {
+    $status = 200;
+  }
+  return ['data' => $result, 'status' => $status];
 }
